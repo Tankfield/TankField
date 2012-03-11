cfc14111489a77303cef541f23b96892fb27ef17
Slavi Sotirov <slavi.sotirov@gmail.com>
2012-03-11 21:32:29 +0200

    Modified main to draw tank object and compiled

--- TankField/main.cpp
+++ TankField/main.cpp
@@ -2,13 +2,15 @@
 
 #include "Application.h"
 #include "Background.h"
+#include "Object.h"
 
 int main(int argc, char* args[] ){
 
 	Application *app = new Application();
 	Background *bg = new Background(app->getScreen());
 	bg->draw(0,0);
-
+	Object obj(new Texture("tank.png", app->getScreen()));
+	obj.texture->draw(0,0);
 	app->Execute();
 	
 	return 0;
